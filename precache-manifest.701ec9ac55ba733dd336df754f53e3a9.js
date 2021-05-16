97d4dd8409dc4c859da9e499d54fbf889a2babf9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-16 02:46:37 +0000

    Deploy omarbastos/vue-weather to github.com/omarbastos/vue-weather.git:gh-pages

---
+++ precache-manifest.701ec9ac55ba733dd336df754f53e3a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e596a1d0c735a25e23f3",
+    "url": "/vue-weather/css/app.f0baf637.css"
+  },
+  {
+    "revision": "5a9c48bb0fb279d3e929",
+    "url": "/vue-weather/css/chunk-vendors.67e288e7.css"
+  },
+  {
+    "revision": "5ce594ea22bebb86a66e29612854d24a",
+    "url": "/vue-weather/img/59910.5ce594ea.jpg"
+  },
+  {
+    "revision": "a14365e962d8e8660c5099aa2be1b0c5",
+    "url": "/vue-weather/img/art-science.a14365e9.jpg"
+  },
+  {
+    "revision": "e9cda87d84f289ea3d27aed7091f4970",
+    "url": "/vue-weather/img/bogota.e9cda87d.png"
+  },
+  {
+    "revision": "e2adfb6b133e3b00fba8c212d15ac3a5",
+    "url": "/vue-weather/img/fountain.e2adfb6b.jpg"
+  },
+  {
+    "revision": "cd68ff67634be5b1b87a17d1c6475652",
+    "url": "/vue-weather/img/san-andres.cd68ff67.jpg"
+  },
+  {
+    "revision": "cba65fe753d42d63cd3940fb90026fa5",
+    "url": "/vue-weather/index.html"
+  },
+  {
+    "revision": "e596a1d0c735a25e23f3",
+    "url": "/vue-weather/js/app.fe4d737c.js"
+  },
+  {
+    "revision": "5a9c48bb0fb279d3e929",
+    "url": "/vue-weather/js/chunk-vendors.db142019.js"
+  },
+  {
+    "revision": "abb7651b3fb33cbb939069d3e6d6a7cc",
+    "url": "/vue-weather/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-weather/robots.txt"
+  }
+]);
